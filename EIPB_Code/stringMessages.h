b19fae9f0480d0e17e4116909710e4a4d9276250
shreyas3096 <83284862+shreyas3096@users.noreply.github.com>
2021-05-10 15:39:08 -0400

    Delete EIPB_Code directory

--- EIPB_Code/stringMessages.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef STRINGMESSAGES_H_
-#define STRINGMESSAGES_H_
-
-	// TODO
-	// Implement HashMap to store string values based on id and error code
-	// getter only
-
-#endif /* STRINGMESSAGES_H_ */
